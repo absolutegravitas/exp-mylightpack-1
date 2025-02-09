63da1dfcf2482c279d1c4e8d6c56d274ff5b42f8
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2025-02-09 17:49:40 +1100

    add basic config for Clerk Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,20 @@
 import "styles/tailwind.css"
-
+import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
+import { PostHogProvider } from "./provider"
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <PostHogProvider>{children}</PostHogProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
