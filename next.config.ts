f4834e1ad48d940fa85e9b4a366afcd8f78a06ad
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2025-02-09 17:42:04 +1100

    add millionlint but keep disabled

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,5 @@
+// next.config.ts
+// import MillionLint from "@million/lint"
 import withBundleAnalyzer from "@next/bundle-analyzer"
 import { type NextConfig } from "next"
 
@@ -16,6 +18,20 @@ const config: NextConfig = {
     { source: "/health", destination: "/api/health" },
     { source: "/ping", destination: "/api/health" },
   ],
+  // webpack: (config, { dev, isServer }) => {
+
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     use: ["@svgr/webpack"],
+  //   })
+  //   return config
+  // },
 }
 
 export default env.ANALYZE ? withBundleAnalyzer({ enabled: env.ANALYZE })(config) : config
+
+// // breaks the runtime, causes LOT LONGER build times
+// export default MillionLint.next({
+//   enabled: true,
+//   rsc: true,
+// })(env.ANALYZE ? withBundleAnalyzer({ enabled: env.ANALYZE })(config) : config)
