6d0dd23f92e0bd85f1833edf7ef8e0b21d730626
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2025-02-09 17:49:20 +1100

    add env type rules

--- env.mjs
+++ env.mjs
@@ -1,3 +1,4 @@
+// env.mjs
 import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
@@ -8,8 +9,24 @@ export const env = createEnv({
       .optional()
       .transform((value) => value === "true"),
   },
-  client: {},
+  client: {
+    ANALYZE: z
+      .enum(["true", "false"])
+      .optional()
+      .transform((value) => value === "true"),
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1), // Add client-side validation
+    NEXT_PUBLIC_POSTHOG_KEY: z.string().min(1), // Add client-side validation
+    NEXT_PUBLIC_POSTHOG_HOST: z.string().min(1), // Add client-side validation
+  },
   runtimeEnv: {
     ANALYZE: process.env.ANALYZE,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   },
+  /**
+   * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
+   * `SOME_VAR=''` will throw an error.
+   */
+  emptyStringAsUndefined: true,
 })
