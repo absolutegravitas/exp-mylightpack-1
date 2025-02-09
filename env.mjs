bebb9f5c650627047c18e37b77aa0cbdc82f1365
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2025-02-09 18:33:29 +1100

    NEXT_PUBLIC_SERVER_URL parameter

--- env.mjs
+++ env.mjs
@@ -14,15 +14,17 @@ export const env = createEnv({
       .enum(["true", "false"])
       .optional()
       .transform((value) => value === "true"),
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1), // Add client-side validation
-    NEXT_PUBLIC_POSTHOG_KEY: z.string().min(1), // Add client-side validation
-    NEXT_PUBLIC_POSTHOG_HOST: z.string().min(1), // Add client-side validation
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+    NEXT_PUBLIC_POSTHOG_KEY: z.string().min(1),
+    NEXT_PUBLIC_POSTHOG_HOST: z.string().min(1),
+    // NEXT_PUBLIC_SERVER_URL: z.string().min(1),
   },
   runtimeEnv: {
     ANALYZE: process.env.ANALYZE,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
     NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    // NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
   },
   /**
    * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
