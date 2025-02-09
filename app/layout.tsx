a90e610757276e5dc581c2bab3941a0b98c43a74
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2025-02-09 20:59:29 +1100

    disable ClerkUI components coz they slowdown mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,22 @@
 import "styles/tailwind.css"
-import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
+// import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
 import { PostHogProvider } from "./provider"
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          <PostHogProvider>{children}</PostHogProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+    // <ClerkProvider>
+    <html lang="en">
+      <body>
+        <PostHogProvider>
+          {/* <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn> */}
+          {children}
+        </PostHogProvider>
+      </body>
+    </html>
+    // </ClerkProvider>
   )
 }
