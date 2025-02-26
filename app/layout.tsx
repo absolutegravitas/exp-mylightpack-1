5c66b403c39604f9b73f73b96faa0a1c97c80e8c
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2025-02-26 16:17:25 +1100

    start adding Clerk Auth Support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,23 @@
 import "styles/tailwind.css"
-// import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
+import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
 import { PostHogProvider } from "./provider"
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    // <ClerkProvider>
-    <html lang="en">
-      <body>
-        <PostHogProvider>
-          {/* <SignedOut>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <PostHogProvider>
+            <SignedOut>
               <SignInButton />
             </SignedOut>
             <SignedIn>
               <UserButton />
-            </SignedIn> */}
-          {children}
-        </PostHogProvider>
-      </body>
-    </html>
-    // </ClerkProvider>
+            </SignedIn>
+            {children}
+          </PostHogProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
