d886dda59c35596b526aa27770bad75c3b226b11
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2025-02-27 00:09:14 +1100

    update ui components

--- components/cookie-consent.tsx
+++ components/cookie-consent.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import { CookieIcon } from "lucide-react"
-import { Button } from "@/components/ui/button"
 import { useEffect, useState } from "react"
+import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 
 export function CookieConsent({
