1d53833e4ab9883ef3b654dbc3076459f7a83206
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2025-02-26 20:46:17 +1100

    remove specific pnpm version in package manager

--- app/gear/page.tsx
+++ app/gear/page.tsx
@@ -1,3 +1,4 @@
+import { SignedIn } from "@clerk/nextjs"
 import React from "react"
 import AddGearItemSection from "@/components/gear-inventory/AddGearItemSection"
 import EditGearItemSection from "@/components/gear-inventory/EditGearItemSection"
@@ -6,10 +7,12 @@ import { Container } from "@/components/ui/container"
 
 export default function GearInventoryPage() {
   return (
-    <Container className="flex flex-col items-center justify-center">
-      <AddGearItemSection />
-      <ViewGearInventorySection />
-      <EditGearItemSection />
-    </Container>
+    <SignedIn>
+      <Container className="flex flex-col items-center justify-center">
+        <AddGearItemSection />
+        <ViewGearInventorySection />
+        <EditGearItemSection />
+      </Container>
+    </SignedIn>
   )
 }
