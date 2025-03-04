c0f186104bbbd129fe55ee0bd9c8998b591b4c43
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2025-03-04 21:38:22 +1100

    add demo route

---
+++ app/demo/layout.tsx
@@ -0,0 +1,16 @@
+import "styles/tailwind.css"
+import { Geist, Geist_Mono } from "next/font/google"
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+})
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+})
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return <section className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</section>
+}
