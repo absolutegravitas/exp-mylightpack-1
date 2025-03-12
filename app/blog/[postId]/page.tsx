f7b7b77b2e8e9074cd54f5b28d5503cd8fa65ec5
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2025-03-12 23:16:30 +1100

    remove some components

--- app/blog/[postId]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function BlogPostDetailPage() {
-  return (
-    <div>
-      <h1>Blog Post Detail Page</h1>
-      <p>This is the blog post detail page content.</p>
-    </div>
-  )
-}
